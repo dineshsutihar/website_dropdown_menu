83b5cd5e0889dbda4ae6274ab1a4bb156e7f43c4
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-28 04:32:31 +0530

    Update menuOpen and menuClose styles and add login functionality

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const menuOpen =document.querySelector('.menu-open');
 const navContent =document.querySelector('.nav-content');
 const mainbody =document.querySelector('main');
 let overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+const login = document.querySelector('.login');
+
 
 menuOpen.addEventListener('click', () => {
     // changing the display property of menuOpen and menuClose
     menuOpen.style.opacity = '0';
     menuOpen.style.display = 'none';
     menuClose.style.opacity = '1';
+    login.style.opacity = '1';
     menuClose.style.display = 'block';
     navContent.style.opacity = "1";
-    overlay.style.display = "block";
+    body.classList.add('overlay');
+    // overlay.style.display = "block";
 });
 
 menuClose.addEventListener('click', () => {
@@ -20,7 +25,10 @@ menuClose.addEventListener('click', () => {
     menuOpen.style.display = 'block';
     menuClose.style.opacity = '0';
     menuClose.style.display = 'none';
+    login.style.opacity = '0';
     navContent.style.opacity = "0";
-    overlay.style.display = "none";
+    body.classList.remove('overlay');
+
+    // overlay.style.display = "none";
 });
 
